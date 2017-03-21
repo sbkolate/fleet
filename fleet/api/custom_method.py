6bab9cdd4fb9b5e26c7e767db66ff1a370b44177
sagar <sagar@digitalprizm.net>
2017-03-21 16:26:42 +0530

    updated api

---
+++ fleet/api/custom_method.py
@@ -0,0 +1,14 @@
+
+from __future__ import unicode_literals
+import frappe, os, json
+from frappe.utils import cstr
+from unidecode import unidecode
+
+@frappe.whitelist(allow_guest=True)
+def update_location(vehicle=None,lat=None,lon=None):
+	vehicle = frappe.get_doc("Vehicle", vehicle)
+	vehicle.latitude = lat
+	vehicle.longitude = lon
+	vehicle.save()
+	frappe.db.commit()
+	return "updated location for " + vehicle.name
