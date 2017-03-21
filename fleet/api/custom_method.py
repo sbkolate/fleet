aedfb7c18e73a5f6a202390d34526d74311bbed6
sagar <sagar@digitalprizm.net>
2017-03-21 18:12:16 +0530

    sas

--- fleet/api/custom_method.py
+++ fleet/api/custom_method.py
@@ -7,8 +7,9 @@ from unidecode import unidecode
 @frappe.whitelist(allow_guest=True)
 def update_location(vehicle=None,lat=None,lon=None):
 	vehicle = frappe.get_doc("Vehicle", vehicle)
+	vehicle.flags.ignore_permissions = True
 	vehicle.latitude = lat
 	vehicle.longitude = lon
-	vehicle.save()
+	vehicle.save(ignore_permissions=True)
 	frappe.db.commit()
 	return "updated location for " + vehicle.name
